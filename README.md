995c73dec5e3f6f548f1bcab525ad6b20c5a2aee
Evgenii Vasiliev <eugene.unn@gmail.com>
2019-09-05 11:51:00 +0300

    Upgrade to Core API
    Upgrade code to Core API

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# PPAM_OpenVINO_tutorial
+# PPAM OpenVINO tutorial
+
+## To run sample, run ```object_detection.py``` with needed arguments.
+
+
+python object_detection.py -i ../data/input -m ../models/ssd300.xml -w ../models/ssd300.bin -c ../models/pascal_voc_classes.txt -l "C:/Program Files (x86)/IntelSWTools/openvino/inference_engine/bin/intel64/Release/cpu_extension_avx2.dll" -em async
--- src/object_detection.py
+++ src/object_detection.py
@@ -4,8 +4,9 @@ import sys
 import argparse
 import numpy as np
 import logging as log
+import copy
 from itertools import product
-from openvino.inference_engine import IENetwork, IEPlugin
+from openvino.inference_engine import IENetwork, IECore
 
 def build_argparser():
     parser = argparse.ArgumentParser()
@@ -37,27 +38,27 @@ def prepare_model(model, weights, cpu_extension, device, plugin_dirs, log):
     model_xml = model
     model_bin = weights
 
-    log.info('Creating plugin object for inference')
-    plugin = IEPlugin(device=device, plugin_dirs=plugin_dirs)
+    log.info('Creating IECore object for inference')
+    ie = IECore()
     if cpu_extension and 'CPU' in device:
-        plugin.add_cpu_extension(cpu_extension)
+        ie.add_extension(cpu_extension, "CPU")
 
     log.info('Loading network files:\n\t{}\n\t{}'.format(model_xml, model_bin))
     net = IENetwork(model=model_xml, weights=model_bin)
 
-    log.info('Checking layers are supported by plugin')
-    if 'CPU' in plugin.device:
-        supported_layers = plugin.get_supported_layers(net)
+    log.info('Checking layers are supported by IECore')
+    if 'CPU' in device:
+        supported_layers = ie.query_network(net, "CPU")
         not_supported_layers = [l for l in net.layers.keys() \
             if l not in supported_layers]
         if not_supported_layers:
-            log.error('Following layers are not supported by the plugin for specified' 
-                'device {}:\n {}'.format(plugin.device, ', '.join(not_supported_layers)))
+            log.error('Following layers are not supported by the IECore for specified' 
+                'device {}:\n {}'.format(ie.device, ', '.join(not_supported_layers)))
             log.error('Please try to specify cpu extensions library path in sample\'s' 
                 'command line parameters using -l or --cpu_extension command line'
                 'argument')
             sys.exit(1)
-    return net, plugin
+    return net, ie
 
 
 def get_image_list(input, log):
@@ -68,17 +69,16 @@ def get_image_list(input, log):
         for file in os.listdir(input[0]):
             if file.endswith(extensions):
                 data.append(os.path.join(input[0], file))
-                return data
     else:
         data = input
     return data
 
-def convert_image(net, data, log):
+def convert_images(net, data, log):
     log.info('Getting shape of input tensor')
     n, c, h, w = net.inputs[next(iter(net.inputs))].shape
     log.info('Creating input images tensor')
     images = np.ndarray(shape=(len(data), c, h, w))
-    for i in range(n):
+    for i in range(len(data)):
         image = cv2.imread(data[i])
         if image.shape[:-1] != (h, w):
             image = cv2.resize(image, (w, h))
@@ -86,15 +86,48 @@ def convert_image(net, data, log):
         images[i] = image
     return images
 
-def infer_sync(image, exec_net, net):
+def infer_sync(images, exec_net, net):
+    results = []
     input_blob = next(iter(net.inputs))
     out_blob = next(iter(net.outputs))
-    res = exec_net.infer(inputs={input_blob : image})
-    return res[out_blob]
+    for image in images:
+        res = exec_net.infer(inputs={input_blob : image})
+        results.append(res[out_blob][0])
+    return results
+
+def infer_async(images, exec_net, net):
+    number_iter = len(images)
+    curr_request_id = 0
+    prev_request_id = 1
+    results = []
+    input_blob = next(iter(net.inputs))
+    out_blob = next(iter(net.outputs))
+    first = True
+    
+    for j in range(number_iter):
+        exec_net.start_async(request_id = curr_request_id, 
+                             inputs = {input_blob: images[j]})
+        if first == True:
+            first = False
+            prev_request_id, curr_request_id = curr_request_id, prev_request_id
+            continue
+        
+        if exec_net.requests[prev_request_id].wait(-1) == 0:
+            results.append(copy.copy(exec_net.requests[prev_request_id].
+                outputs[next(iter(net.outputs))][0]))
+        prev_request_id, curr_request_id = curr_request_id, prev_request_id
+    
+    if exec_net.requests[prev_request_id].wait(-1) == 0:
+        results.append(copy.copy(exec_net.requests[prev_request_id].
+            outputs[next(iter(net.outputs))][0]))
+        
+    return results
 
 def detection_output(input_data, output_folder, pred, classes_file, log):
     classes = [line.rstrip('\n') for line in open(classes_file)]
-    threshold = 0.3
+    threshold = 0.5
+
+    print(len(pred), pred[0].shape)
 
     for im_id, data in enumerate(pred):
         img_name = input_data[im_id]
@@ -106,11 +139,11 @@ def detection_output(input_data, output_folder, pred, classes_file, log):
                 cv2.rectangle(img,
                           (int(data[0,i,3] * w), int(data[0,i,4] * h)),
                           (int(data[0,i,5] * w), int(data[0,i,6] * h)),
-                          (0,255,0),3)
+                          (0,255,0),1)
                 cv2.putText(img, classes[int(data[0,i,1]-1)], 
                            (int(data[0,i,3] * w), int(data[0,i,6] * h)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 
-                           0.8, (0, 0, 255), 2, cv2.LINE_AA)
+                           0.8, (0, 255, 255), 2, cv2.LINE_AA)
         out_img = os.path.join(output_folder, os.path.basename(img_name))
         cv2.imwrite(out_img, img)
         log.info('Result image was saved to {}'.format(out_img))
@@ -121,25 +154,32 @@ def main():
         level=log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
     try:
-        net, plugin = prepare_model(args.model, args.weights,
+        net, ie = prepare_model(args.model, args.weights,
             args.cpu_extension, args.device, args.plugin_dir, log)
 
         data = get_image_list(args.input, log)
-        images = convert_image(net, data, log)
+        images = convert_images(net, data, log)
 
-        log.info('Loading model to the plugin')
-        exec_net = plugin.load(network=net)
+        log.info('Loading model to the core')
+        if args.execution_mode == 'async':
+            exec_net = ie.load_network(network=net, device_name=args.device, 
+                                       num_requests = 2)
+        else:
+            exec_net = ie.load_network(network=net, device_name=args.device)
 
         log.info('Starting inference')
-        res = infer_sync(images, exec_net, net)
+        if args.execution_mode == 'async':
+            res = infer_async(images, exec_net, net)
+        else:
+            res = infer_sync(images, exec_net, net)
 
         log.info('Processing model output')
-        res_images = detection_output(data, args.output_folder, res, args.classes, log)
+        detection_output(data, args.output_folder, res, args.classes, log)
 
         log.info('Free memory')
         del net
         del exec_net
-        del plugin
+        del ie
     except Exception as ex:
         log.error(str(ex))
         sys.exit(1)
